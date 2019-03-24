2bf53c38fd569e71f12125d6c4b3f5f92cfa96f2
Yuichi Hagio <yhagio87@gmail.com>
2019-03-23 22:48:58 -0400

    Updated sql

--- init.sql
+++ init.sql
@@ -1,4 +1,5 @@
-CREATE DATABASE sensei_dev;
-CREATE user dev_user with password 'test123';
-grant all privileges ON DATABASE sensei_dev TO dev_user;
+CREATE user dev_user with password '123test';
 ALTER USER dev_user with superuser;
+
+CREATE DATABASE sensei_dev;
+GRANT all privileges ON DATABASE sensei_dev TO dev_user;
--- src/seed/db.test.sql
+++ src/seed/db.test.sql
@@ -1,6 +1,5 @@
 CREATE database sensei_test;
-CREATE user sensei_test_user with password 'password';
-GRANT all privileges on database sensei_test to sensei_test_user;
-ALTER USER sensei_test_user with superuser;
+
+GRANT all privileges on database sensei_test to dev_user;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
